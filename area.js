a22d5741b962318bb9ec433f6b71943c7c1c3898
godwillboro-source <godwillboro@gmail.com>
2026-06-22 09:58:01 +0300

    circle area

--- area.js
+++ area.js
@@ -2,3 +2,7 @@ function calculateArea(length, width) {
     return length * width;
 }
 console.log(calculateArea(5, 10)); // Output: 50
+function calculateCircleArea(radius) {
+    return 3.142 * radius * radius;
+}
+console.log(calculateCircleArea(7)); // Output: 153.93804002589985
