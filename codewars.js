be512a4c5d747131aa00daefa586d92618cf4b9c
godwillboro-source <godwillboro@gmail.com>
2026-06-26 14:47:36 +0300

    food order function

--- codewars.js
+++ codewars.js
@@ -14,9 +14,33 @@ function mostFrequentItemCount(collection) {
 console.log(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3, 9, 9, 9, 9, 9, 9, 9]));
 
 //list filtering
-//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
 function filter_list(l) {
   // Return a new array with the strings filtered out
-  return l.filter(item => typeof item === "number")
+  return l.filter(item => typeof item === "number");
 }
-console.log(filter_list([1, 2, "a", "b", 3])); // Output: [1, 2, 3]
+console.log(filter_list([1, 2, "a", "b", 3, true, false, 5, "5"])); // Output: [1, 2, 3, 5]
+
+function orderFood(list) {
+  // thank you for checking out the Coding Meetup kata :)
+  const result = {};
+  const mealTypes = ['standard', 'vegetarian', 'vegan', 'diabetic', 'gluten-intolerant'];
+  
+  for (let i = 0; i < mealTypes.length; i++) {
+    const mealType = mealTypes[i];
+    const count = list.filter(developer => developer.meal === mealType).length;
+    result[mealType] = count;
+  }
+  return result;
+} 
+const list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+];
+console.log(orderFood(list1));
