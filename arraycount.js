bc206cfc8090e29e87ed5ad4e8ee4f18b3295340
godwillboro-source <godwillboro@gmail.com>
2026-06-26 11:55:28 +0300

    array filtering

--- arraycount.js
+++ arraycount.js
@@ -11,3 +11,11 @@ function mostFrequentItemCount(collection) {
   return Math.max(...Object.values(counts));
 }
 console.log(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3, 9, 9, 9, 9, 9, 9, 9]));
+
+//list filtering
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(item => typeof item === "number")
+}
+console.log(filter_list([1, 2, "a", "b", 3])); // Output: [1, 2, 3]
